1e826bd9a5e1c1723138532f9847137b1fc6bb84
zack xue <zackxue@Zacks-MacBook-Pro-14inch.local>
2023-10-15 01:17:36 -0700

    init ms2

---
+++ Milestone2_Controller/src/model/world/World.java
@@ -0,0 +1,88 @@
+package model.world;
+
+import java.awt.image.BufferedImage;
+import java.util.List;
+
+/**
+ * The World interface represents the game world, which consists of rooms and entities.
+ * It provides various methods for interacting with the game world.
+ */
+public interface World {
+
+  /**
+   * Gets a list of neighboring rooms for a given room name.
+   *
+   * @param roomName The name of the room.
+   * @return A list of neighboring room names.
+   * @throws IllegalArgumentException If the room name is not valid.
+   * @throws NullPointerException     If the provided room name is null.
+   */
+  List<String> getNeighborsRoomList(String roomName)
+      throws IllegalArgumentException, NullPointerException;
+
+  /**
+   * Gets the name of the world.
+   *
+   * @return The name of the world.
+   */
+  String getWorldName();
+
+  /**
+   * Gets the total number of rooms in the world.
+   *
+   * @return The total number of rooms.
+   */
+  int getTotalOfRoom();
+
+  /**
+   * Gets the total number of items in the world.
+   *
+   * @return The total number of items.
+   */
+  int getTotalOfItem();
+
+  /**
+   * Gets information about a specific room in the world.
+   *
+   * @param roomName The name of the room.
+   * @return Information about the room.
+   * @throws IllegalArgumentException If the room name is not valid.
+   * @throws NullPointerException     If the provided room name is null.
+   */
+  String getOneRoomInfo(String roomName)
+      throws IllegalArgumentException, NullPointerException;
+
+  /**
+   * Moves Dr. Lucky within the world.
+   */
+  void moveDrLucky();
+
+  /**
+   * Creates a graphical representation of the world as a BufferedImage.
+   *
+   * @return An image representation of the world.
+   */
+  BufferedImage createGraphBufferedImage();
+
+  /**
+   * Prints the neighbor map of the world to the console.
+   */
+  void printWorldNeighborMap();
+
+  /**
+   * Prints a 2D array representation of the world to the console.
+   */
+  void printWorld2dArray();
+
+  /**
+   * Gets information about Dr. Lucky within the world.
+   *
+   * @return Information about Dr. Lucky.
+   */
+  String getDrLuckyInfo();
+
+  /**
+   * Prints information about all rooms in the world to the console.
+   */
+  void printAllRoomInfo();
+}
